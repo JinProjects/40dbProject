e7292c77021617d7181e382f3dfaad3f38f244aa
DESKTOP-LVECPOO\tj-bu-703-23 <jhyukcode@gmail.com>
2025-04-28 12:56:12 +0900

    04.28

--- 40db/src/main/java/com/db40/library/member/MemberStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.db40.library.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberStatusRepository extends JpaRepository<MemberStatus, Integer>{
-
-}
